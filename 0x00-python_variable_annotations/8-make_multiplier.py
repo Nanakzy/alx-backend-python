97d53d62b2e38703ab40ff7da46cdeef06c5d1d0
Nanakzy <nanakzy@gmail.com>
2024-07-04 18:17:38 +0000

    committing

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns a multiplier function.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by the given multiplier.
+
+    Parameters:
+    multiplier (float): The multiplier.
+
+    Returns:
+    Callable[[float], float]: A function that multiplies a float 
+    by the multiplier.
+    """
+    def multiply_by(n: float) -> float:
+        """
+        Multiplies a float by the multiplier.
+
+        Parameters:
+        n (float): The float to multiply.
+
+        Returns:
+        float: The result of the multiplication.
+        """
+        return n * multiplier
+    
+    return multiply_by
